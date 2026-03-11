dde535ea3bf8b33dd9ed80dba6ba21ffb03b5f45
ANIMASHAUN Michael <michaeloncode@gmail.com>
2026-03-11 17:52:07 +0100

    feat: reports page using AnalyticsReportDownload implemented

---
+++ app/reports/[tenantKey]/[reportName]/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import { AnalyticsReportDownload } from "@iblai/iblai-js/web-containers";
+import { useParams } from "next/navigation";
+
+export default function ReportDownloadPage() {
+  const { tenantKey, reportName } = useParams<{
+    tenantKey: string;
+    reportName: string;
+  }>();
+
+  return (
+    <div className="h-full overflow-hidden">
+      <AnalyticsReportDownload
+        platform_key={tenantKey}
+        report_name={reportName}
+      />
+    </div>
+  );
+}
