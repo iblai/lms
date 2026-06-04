483e32409aacc044f089d731e73f8732c404b51e
ANIMASHAUN Michael <michaeloncode@gmail.com>
2026-06-04 18:28:53 +0100

    feat: app pages restructuring with tenant key on the url

--- components/self-linking-guard.tsx
+++ components/self-linking-guard.tsx
@@ -2,6 +2,7 @@
 
 import type React from 'react';
 import { useEffect } from 'react';
+// @ts-ignore
 import { useGetPublicPlatformMembershipQuery } from '@iblai/iblai-js/data-layer';
 import { isLoggedIn } from '@iblai/iblai-js/web-utils';
 import { useTenantParam } from '@/hooks/use-tenant-param';
